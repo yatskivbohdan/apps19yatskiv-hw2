6c4e0d5cbce43d117500c13226defc8bd5d109fd
bohdan <yatskivbohdan@gmail.com>
2019-11-03 18:00:11 +0200

    Added Queue and Stack tests

--- src/test/java/ua/edu/ucu/collections/QueueTest.java
+++ src/test/java/ua/edu/ucu/collections/QueueTest.java
@@ -1,12 +1,36 @@
 package ua.edu.ucu.collections;
 
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class QueueTest {
-    
+
+    private Queue queue;
+
+    @Before
+    public void setUp(){
+        queue = new Queue();
+    }
+
     @Test
-    public void testSomeMethod() {
+    public void testEnqueue(){
+        for(int i = 0; i<10; i++){
+            queue.enqueue(i);
+            assertEquals(queue.peek(), 0);
+        }
+    }
+
+    @Test
+    public void testDequeue(){
+        for(int i=0; i<10; i++){
+            queue.enqueue(i);
+        }
+        for(int i=9; i>1; i--){
+            Object peek = queue.peek();
+            Object item = queue.dequeue();
+            assertEquals(peek, item);
+        }
     }
     
 }
--- src/test/java/ua/edu/ucu/collections/StackTest.java
+++ src/test/java/ua/edu/ucu/collections/StackTest.java
@@ -1,12 +1,36 @@
 package ua.edu.ucu.collections;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class StackTest {
-    
-    @Test
-    public void testSomeMethod() {
+    private Stack stack;
+
+    @Before
+    public void setUp(){
+        stack = new Stack();
     }
-    
+
+    @Test
+    public void testPush(){
+        for(int i=0; i<10; i++){
+            stack.push(i);
+            assertEquals(stack.peek(), i);
+        }
+        assertEquals(stack.peek(), 9);
+    }
+
+    @Test
+    public void testPop(){
+        for(int i=0; i<10; i++){
+            stack.push(i);
+        }
+        for(int i=9; i>1; i--){
+            Object peek = stack.peek();
+            Object item = stack.pop();
+            assertEquals(peek, item);
+        }
+    }
 }
