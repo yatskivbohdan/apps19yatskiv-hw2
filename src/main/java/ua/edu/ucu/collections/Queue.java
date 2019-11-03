03d5f065c448a9892f2c6e5767769d3447c8a15a
bohdan <yatskivbohdan@gmail.com>
2019-11-03 17:14:07 +0200

    Added Queue and Stack classes

--- src/main/java/ua/edu/ucu/collections/Queue.java
+++ src/main/java/ua/edu/ucu/collections/Queue.java
@@ -1,5 +1,25 @@
 package ua.edu.ucu.collections;
 
+import ua.edu.ucu.collections.immutable.ImmutableLinkedList;
+
 public class Queue {
-    
+    private ImmutableLinkedList list;
+
+    public Queue(){
+        list = new ImmutableLinkedList();
+    }
+
+    public Object peek(){
+        return list.getFirst();
+    }
+
+    public void enqueue(Object item){
+        list = list.addLast(item);
+    }
+
+    public Object dequeue(){
+        Object item = list.getFirst();
+        list = list.removeFirst();
+        return item;
+    }
 }
--- src/main/java/ua/edu/ucu/collections/Stack.java
+++ src/main/java/ua/edu/ucu/collections/Stack.java
@@ -1,5 +1,25 @@
 package ua.edu.ucu.collections;
 
+import ua.edu.ucu.collections.immutable.ImmutableLinkedList;
+
 public class Stack {
-    
+    private ImmutableLinkedList list;
+
+    public Stack(){
+        list = new ImmutableLinkedList();
+    }
+
+    public Object peek(){
+        return list.getFirst();
+    }
+
+    public void push(Object item){
+        list = list.addFirst(item);
+    }
+
+    public Object pop(){
+        Object item = list.getFirst();
+        list = list.removeFirst();
+        return item;
+    }
 }
